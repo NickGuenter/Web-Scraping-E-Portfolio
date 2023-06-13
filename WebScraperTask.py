d47bb2f7020fdd386fc74b3be06199e149bb0509
NickGuenter <98540299+NickGuenter@users.noreply.github.com>
2023-06-13 19:04:40 +0200

    Changes

---
+++ WebScraperTask.py
@@ -0,0 +1,18 @@
+# Importing the required libraries
+import requests
+from bs4 import BeautifulSoup
+
+# URL for scrapping data
+url = "http://quotes.toscrape.com/"
+
+# Sending request to the website and getting the response back
+
+# Parse HTML code for the entire site using Beautiful Soup
+
+# Get all quotes and authors
+
+# Print out the quotes and corresponding authors
+
+# For more Swag:
+# If you like, you can also create a list for quotes and a list for authors
+# and zip them together (Hint: use zip() function) to get a list of tuples and print them out
